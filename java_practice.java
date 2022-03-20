0d047a8cca71bd70aecc579815b07d3e48884f2a
sonjerry <101078893+sonjerry@users.noreply.github.com>
2022-03-20 18:15:29 +0900

    master

---
+++ java_practice.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class java_practice {
+    public static void main(String[] args) {
+        double b = 0;
+        System.out.println("hello!");
+
+
+        double c = 0;
+        Scanner scan1 = new Scanner(System.in);
+        Scanner scan2 = new Scanner(System.in);
+        b = scan1.nextDouble();
+        c = scan2.nextDouble();
+
+        double num1=(-b+Math.sqrt(b*b-4*c))/2;
+        double num2=(-b+Math.sqrt(b*b-4*c))/2;
+        System.out.println(num1);
+        System.out.println(num2);
+        scan1.close();
+        scan2.close();
+    }
+}
+
+    
+
